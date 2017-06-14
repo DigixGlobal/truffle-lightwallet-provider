0b3bce9f34ca5aa2b8cf4a0a7a7732d9e48572bf
Chris Hitchcott <hitchcott@gmail.com>
2017-06-14 17:11:41 +0900

    [maint] simply subprovider, bump libs

--- src/lightwalletSubprovider.js
+++ src/lightwalletSubprovider.js
@@ -1,19 +1,30 @@
 import HookedWalletEthTx from 'web3-provider-engine/subproviders/hooked-wallet-ethtx';
+import EthTx from 'ethereumjs-tx';
+import ethUtil from 'ethereumjs-util';
 
 export default class LightwalletSubprovider extends HookedWalletEthTx {
   constructor({ password, ks, addresses }) {
     super({
       getAccounts(cb) {
-        return cb(null, addresses);
-      },
-      getPrivateKey(address, cb) {
-        ks.keyFromPassword(password, (err, pwDerivedKey) => {
-          if (err) { return cb(err); }
-          const privateKey = ks.exportPrivateKey(address, pwDerivedKey);
-          const buf = Buffer.from(privateKey, 'hex');
-          return cb(null, buf);
-        });
+        // console.log(...addresses);
+        return cb(null, addresses.slice(0));
       },
     });
+    this.signTransaction = function(txData, cb) {
+      if (txData.gas !== undefined) txData.gasLimit = txData.gas
+      txData.value = txData.value || '0x00'
+      txData.data = ethUtil.addHexPrefix(txData.data)
+
+      ks.keyFromPassword(password, (err, pwDerivedKey) => {
+        if (err) { return cb(err); }
+        const privateKey = ks.exportPrivateKey(txData.from, pwDerivedKey);
+        // console.log('got pk', privateKey);
+        const buf = Buffer.from(privateKey, 'hex');
+        // return cb(null, buf);
+        var tx = new EthTx(txData)
+        tx.sign(buf)
+        cb(null, '0x' + tx.serialize().toString('hex'))
+      });
+    }
   }
 }
