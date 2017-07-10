7e467b8737bae8894ddebd200f1c9031e0abe2ec
Chris Hitchcott <hitchcott@gmail.com>
2017-07-10 14:53:47 +0800

    implement web3.eth.sign, update deps, linting fix, bump version

--- src/lightwalletSubprovider.js
+++ src/lightwalletSubprovider.js
@@ -1,19 +1,52 @@
 import HookedWalletEthTx from 'web3-provider-engine/subproviders/hooked-wallet-ethtx';
 import EthTx from 'ethereumjs-tx';
 import ethUtil from 'ethereumjs-util';
+import sigUtil from 'eth-sig-util';
+
+
+function concatSig(rawV, rawR, rawS) {
+  let r = ethUtil.fromSigned(rawR);
+  let s = ethUtil.fromSigned(rawS);
+  let v = ethUtil.bufferToInt(rawV);
+  r = ethUtil.toUnsigned(r).toString('hex');
+  s = ethUtil.toUnsigned(s).toString('hex');
+  v = ethUtil.stripHexPrefix(ethUtil.intToHex(v));
+  return ethUtil.addHexPrefix(r.concat(s, v).toString('hex'));
+}
+
 
 export default class LightwalletSubprovider extends HookedWalletEthTx {
   constructor({ password, ks, addresses }) {
     super({
       getAccounts(cb) {
-        // console.log(...addresses);
         return cb(null, addresses.slice(0));
       },
     });
-    this.signTransaction = function(txData, cb) {
-      if (txData.gas !== undefined) txData.gasLimit = txData.gas
-      txData.value = txData.value || '0x00'
-      txData.data = ethUtil.addHexPrefix(txData.data)
+
+    this.signMessage = function (msgParams, cb) {
+      ks.keyFromPassword(password, (err, privateKey) => {
+        if (err) return cb(err);
+        const msgHash = ethUtil.sha3(msgParams.data);
+        const sig = ethUtil.ecsign(msgHash, privateKey);
+        const serialized = ethUtil.bufferToHex(concatSig(sig.v, sig.r, sig.s));
+        cb(null, serialized);
+      });
+    };
+
+    this.signPersonalMessage = function (msgParams, cb) {
+      ks.keyFromPassword(password, (err, privateKey) => {
+        if (err) return cb(err);
+        const serialized = sigUtil.personalSign(privateKey, msgParams);
+        cb(null, serialized);
+        return null;
+      });
+    };
+
+    this.signTransaction = function (rawTxData, cb) {
+      const txData = { ...rawTxData };
+      if (txData.gas !== undefined) txData.gasLimit = txData.gas;
+      txData.value = txData.value || '0x00';
+      txData.data = ethUtil.addHexPrefix(txData.data);
 
       ks.keyFromPassword(password, (err, pwDerivedKey) => {
         if (err) { return cb(err); }
@@ -21,10 +54,11 @@ export default class LightwalletSubprovider extends HookedWalletEthTx {
         // console.log('got pk', privateKey);
         const buf = Buffer.from(privateKey, 'hex');
         // return cb(null, buf);
-        var tx = new EthTx(txData)
-        tx.sign(buf)
-        cb(null, '0x' + tx.serialize().toString('hex'))
+        const tx = new EthTx(txData);
+        tx.sign(buf);
+        cb(null, `0x${tx.serialize().toString('hex')}`);
+        return null;
       });
-    }
+    };
   }
 }
