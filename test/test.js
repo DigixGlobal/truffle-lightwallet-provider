abd56d4ac557ee4f72c773cd1e42c620fc4db1eb
Chris Hitchcott <hitchcott@gmail.com>
2017-07-20 16:09:30 +0800

    clarify test, bump version

--- test/test.js
+++ test/test.js
@@ -28,9 +28,11 @@ describe('truffle-lightwallet-provider', function () {
     // sha3('xyz')
     const address = '0xef64eb65ab7e265fdfcf1ab93a1292fdc15d9761';
     const hash = '0x9dd2c369a187b4e6b9c402f030e50743e619301ea62aa4c0737d4ef7e10a3d49';
-    const expected = await a.callback(web3Default.eth.sign, address, hash);
-    // const expected = '0x860d6f654853184e1e6d2715902930a5fbc3942d2291c7673fed587ac49f7c733704b6a25df05627b6a5487133b25ad821c2536c1dc77ae7b65550dcea977daa01';
+    const expected = '0x07c84d617380fa6bcebf93f8b144b0fbc452efef893a2a1cd45abe5f56ae31e94c42cd9f597aecd128aff1e48ca834b92ad8df7e44ffba5f00f75bd7bac19f4601';
+    const fromTestRpc = await a.callback(web3Default.eth.sign, address, hash);
     const actual = await a.callback(web3.eth.sign, address, hash);
+    console.log({ fromTestRpc, actual, expected });
+    assert.equal(fromTestRpc, expected, 'testrpc is behaving correctly');
     assert.equal(actual, expected);
   });
 });
